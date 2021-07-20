534f14b11b2861483676e5dd042e3d3b29843a6c
Robbie08 <rortiz8838@sdsu.edu>
2021-07-19 23:35:12 -0700

    Update: added a struct to contain our ScrapedData, and a interface to help with interaction with webcrawler

---
+++ go.mod
@@ -0,0 +1,5 @@
+module webcrawler
+
+go 1.13
+
+require github.com/Robbie08/webcrawler v0.0.0-20210720061211-f51f841d6117 // indirect
--- pkg/crawler/crawler.go
+++ pkg/crawler/crawler.go
@@ -3,13 +3,32 @@ package crawler
 import (
     "fmt"
     "net/http"
+    "github.com/PuerkitoBio/goquery"
 )
 
+
+// Basic layout of object that will
+// contain our scraped data
+type ScrapedData {
+    URL     string
+    Title   string
+    H1      string
+}
+
+
+// let's create an interface to help multi-file data transfer
+type Scraper interface {
+    ScrapePage(*goquery.Document) ScrapedData
+}
+
+// Dummy function to help test if we are still connected to main.go
 func PrintFromCrawler(val string){
     fmt.Println("Hello ", val)
 }
 
-// Grab package using Get request. 
+// Grab package using Get request.
+// This is the start of the scraping process and will help us
+// gain access to the HTML code from the site.
 func getRequest(url string) (*http.Response, error){
     target := &http.Client{}
     req, _ := http.NewRequest("Get", url, nil)
